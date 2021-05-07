751a1f152a94cbe9b1b35207a55e3d1cb9148dc7
Hunter Xia <hunterxia@outlook.com>
2021-05-07 14:00:24 +0800

    Initial Commit

---
+++ ParseIns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParseIns
+//
+//  Created by Hunter Xia on 2021/5/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
